f8349a3202a5cf7376a64570b47ad26cf8aadeac
Ionut <81566145+Blitz-Cloud@users.noreply.github.com>
2023-05-06 12:05:45 +0300

    Update[style]: I updated the card part from the post grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import * as React from "react";
 
 const Header = () => {
   return (
-    <header className="h-[95vh] flex justify-start items-center p-4 ">
-      <div className="backdrop-filter backdrop-blur-md bg-white bg-opacity-10 py-6 px-2 rounded-md text-center sm:text-start sm:px-4 max-w-[720px]">
+    <header className="h-[95vh] flex justify-start items-center p-4  ">
+      <div className="backdrop-filter backdrop-blur-md bg-white bg-opacity-10 py-6 px-2 rounded-md text-center sm:text-start sm:px-4 max-w-[640px] shadow-lg">
         <h1 className="text-5xl">Fizica interactiva</h1>
-        <p className="pl-2 text-xl inline">
+        <p className="pl-2 text-xl  ">
           Descopera experimente si principii din fizica intr o maniera atractiva
           pentru toate varstele....
         </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Header from "../components/Header";
 import PostGrid from "../components/PostGrid";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <div>
       <Header />
       <PostGrid />
+      <Link to="/test">test</Link>
     </div>
   );
 };
